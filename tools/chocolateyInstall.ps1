c04327fa2fde71b311f1e484e64aaad563a4c4e6
MKAbuMattar <mohammad.khaled@outlook.com>
2025-02-03 21:20:14 +0300

    Update Chocolatey package icon URL and refactor installation script for improved argument handling

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,22 +1,16 @@
 $ErrorActionPreference = 'Stop';
-$packageName = 'lsstudio'
 $url = 'https://installers.lmstudio.ai/win32/x64/0.3.9-6/LM-Studio-0.3.9-6-x64.exe'
-$installerType = 'exe'
 $checksum = '1dbc7bafbcc458a628e23316d1b53a9906d0d30dd83673c87f86af1893ee5906'
 $checksumType = 'sha256'
-$silentArgs = '/S'
-$validExitCodes = @(0)
 
-$installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$downloadFile = Join-Path $installPath "$($packageName)Setup.exe"
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = $checksumType
+  silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes = @(0)
+}
 
-Get-ChocolateyWebFile -PackageName $packageName `
-  -FileFullPath $downloadFile `
-  -Url $url `
-  -Checksum $checksum `
-  -ChecksumType $checksumType
-Install-ChocolateyInstallPackage -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs $silentArgs `
-  -File $downloadFile `
-  -ValidExitCodes $validExitCodes
+Install-ChocolateyPackage @packageArgs
