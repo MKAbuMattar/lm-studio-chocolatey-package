052618633a1204c990d9a00a2b7cb462b036ff70
MKAbuMattar <mohammad.khaled@outlook.com>
2025-02-07 23:31:32 +0300

    Update package name in Chocolatey installation script to 'lmstudio'

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
-$packageName = 'Witsy'
+$packageName = 'lmstudio'
 $url = 'https://installers.lmstudio.ai/win32/x64/0.3.9-6/LM-Studio-0.3.9-6-x64.exe'
 $installerType = 'exe'
 $checksum = '1dbc7bafbcc458a628e23316d1b53a9906d0d30dd83673c87f86af1893ee5906'
@@ -11,12 +11,12 @@ $installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $downloadFile = Join-Path $installPath "$($packageName)Setup.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName `
-    -FileFullPath $downloadFile `
-    -Url $url `
-    -Checksum $checksum `
-    -ChecksumType $checksumType
+  -FileFullPath $downloadFile `
+  -Url $url `
+  -Checksum $checksum `
+  -ChecksumType $checksumType
 Install-ChocolateyInstallPackage -PackageName $packageName `
-    -FileType $installerType `
-    -SilentArgs $silentArgs `
-    -File $downloadFile `
-    -ValidExitCodes $validExitCodes
+  -FileType $installerType `
+  -SilentArgs $silentArgs `
+  -File $downloadFile `
+  -ValidExitCodes $validExitCodes
