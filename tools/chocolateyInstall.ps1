de997b6e0dfcdffb83ce42e54509f402d4f2bbd1
MKAbuMattar <mohammad.khaled@outlook.com>
2025-05-14 00:18:41 +0300

    fix: Update silentArgs and validExitCodes in chocolateyInstall.ps1 for improved installation handling

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   checksum64     = '1dbc7bafbcc458a628e23316d1b53a9906d0d30dd83673c87f86af1893ee5906'
   checksumType64 = 'sha256'
   softwareName   = 'LM Studio*'
-  silentArgs     = '/S'
-  validExitCodes = @(0)
+  silentArgs     = '/S /quiet'
+  validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
