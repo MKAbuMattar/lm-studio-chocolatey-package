2ef3740c4a67ab4649e94d9f2379811b87506ccf
MKAbuMattar <mohammad.khaled@outlook.com>
2025-05-14 00:17:45 +0300

    fix: Refactor chocolateyInstall.ps1 to use packageArgs for installation parameters

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,22 +1,16 @@
-$ErrorActionPreference = 'Stop';
-$packageName = 'lmstudio'
-$url = 'https://installers.lmstudio.ai/win32/x64/0.3.9-6/LM-Studio-0.3.9-6-x64.exe'
-$installerType = 'exe'
-$checksum = '1dbc7bafbcc458a628e23316d1b53a9906d0d30dd83673c87f86af1893ee5906'
-$checksumType = 'sha256'
-$silentArgs = '/S'
-$validExitCodes = @(0)
+$ErrorActionPreference = 'Stop'
 
-$installPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$downloadFile = Join-Path $installPath "$($packageName)Setup.exe"
+$toolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Get-ChocolateyWebFile -PackageName $packageName `
-  -FileFullPath $downloadFile `
-  -Url $url `
-  -Checksum $checksum `
-  -ChecksumType $checksumType
-Install-ChocolateyInstallPackage -PackageName $packageName `
-  -FileType $installerType `
-  -SilentArgs $silentArgs `
-  -File $downloadFile `
-  -ValidExitCodes $validExitCodes
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64          = 'https://installers.lmstudio.ai/win32/x64/0.3.9-6/LM-Studio-0.3.9-6-x64.exe'
+  checksum64     = '1dbc7bafbcc458a628e23316d1b53a9906d0d30dd83673c87f86af1893ee5906'
+  checksumType64 = 'sha256'
+  softwareName   = 'LM Studio*'
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
